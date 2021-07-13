b0d155187e7a45ab3d22b424250f2728f2c44f72
Hiner, Christopher <christopher.hiner@jackson.com>
2021-07-13 20:36:43 +0000

    Read more docs

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -7,9 +7,9 @@ async function run() {
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
 
-    const octokit = new github.getOctokit(token);
+    const octokit = github.getOctokit(token);
 
-    const newIssue = await octokit.issues.create({
+    const newIssue = await octokit.rest.issues.create({
         repo: github.context.repo.repo,
         owner: github.context.repo.owner,
         title: issueTitle,
